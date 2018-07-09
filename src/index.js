6813d410f7fba9fa500cd91717661aaa7993cfcb
wal-shahzaib <theshahzaib5@gmail.com>
2018-07-09 15:47:27 +0500

    added a boolean check to check the player

--- src/index.js
+++ src/index.js
@@ -19,13 +19,17 @@ class Board extends React.Component {
 
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -36,7 +40,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X'
+    const status = "Next Player is: " + (this.state.xIsNext ? 'X' : 'O')
 
     return (
       <div className="game-board">
