0dd97cae97a69cffb20dadccf25ade230e0b1b4b
wal-shahzaib <theshahzaib5@gmail.com>
2018-07-09 17:45:40 +0500

    added winner decider function

--- src/index.js
+++ src/index.js
@@ -13,6 +13,28 @@ function Box(props) {
   )
 }
 
+function WinnerSelection(boxes) {
+  const boxLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (let i = 0; i < boxLines.length; i++) {
+    const [a, b, c] = boxLines[i]
+    if (boxes[a] && boxes[b] === boxes[b] && boxes[a] === boxes[c]) {
+      return boxes[a]
+    }
+  }
+
+  return null
+}
+
 class BoxContainer extends React.Component {
   constructor(props) {
     super(props)
